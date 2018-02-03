7bb0f7f9f231ed4934c1c32fa802553b97206bea
alfirohman <33487740+alfirohman@users.noreply.github.com>
2018-02-03 02:44:37 -0800

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,5 +1,13 @@
-<?php
-document.write("<a href='tg://resolve?domain=spulsa_bot' style='color:blue;'>
-<img alt='center telegram pulsa murah' src='//s-pulsa.net/script/telegram-center.png' width='125' height='37'/>
-<br/>@spulsa_bot</a><br/>");
-?>
+document.write("085311962999
+"); document.write("081314145999
+"); document.write("081321537999
+"); document.write("081321538999
+"); document.write("085749964999
+"); document.write("085749972999
+"); document.write("085707255999
+"); document.write("085707084999
+"); document.write("087870797999
+"); document.write("089523374999
+"); document.write("089610835999
+"); document.write("08985888823
+");
