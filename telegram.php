cbdfd8339da6b887c540913949b7fa9b23656cb2
alfirohman <33487740+alfirohman@users.noreply.github.com>
2018-02-03 02:56:49 -0800

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -1,9 +1,14 @@
-<script type='text/javascript'>
-prefix=&quot;<data:content/>&quot;;
-</script>
-
-<script type='text/javascript'>
-  center="document.write("085311962999"); document.write("081314145999"); document.write("081321537999"); document.write("081321538999"); 
-document.write("085749964999"); document.write("085749972999"); document.write("085707255999"); document.write("085707084999"); 
-document.write("087870797999"); document.write("089523374999"); document.write("089610835999"); document.write("08985888823");
+<script language="javascript">
+  document.write("085311962999"); 
+  document.write("081314145999"); 
+  document.write("081321537999"); 
+  document.write("081321538999"); 
+  document.write("085749964999"); 
+  document.write("085749972999"); 
+  document.write("085707255999"); 
+  document.write("085707084999"); 
+  document.write("087870797999"); 
+  document.write("089523374999"); 
+  document.write("089610835999"); 
+  document.write("08985888823");
 </script>
