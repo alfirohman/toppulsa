93008df82e6aecd00916daaa62f4aec5698307a3
alfirohman <33487740+alfirohman@users.noreply.github.com>
2018-02-03 03:10:01 -0800

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,15 +1,14 @@
 <script type='text/javascript'>
-prefix=";
-  document.write("085311962999"); 
-  document.write("081314145999"); 
-  document.write("081321537999"); 
-  document.write("081321538999"); 
-  document.write("085749964999"); 
-  document.write("085749972999"); 
-  document.write("085707255999"); 
-  document.write("085707084999"); 
-  document.write("087870797999"); 
-  document.write("089523374999"); 
-  document.write("089610835999"); 
-  document.write("08985888823");"
+document.write("085311962999<br/>");
+document.write("081314145999<br/>");
+document.write("081321537999<br/>");
+document.write("081321538999<br/>");
+document.write("085749964999<br/>");
+document.write("085749972999<br/>");
+document.write("085707255999<br/>");
+document.write("085707084999<br/>");
+document.write("087870797999<br/>");
+document.write("089523374999<br/>");
+document.write("089610835999<br/>");
+document.write("08985888823<br/>");
 </script>
